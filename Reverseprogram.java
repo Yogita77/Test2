f8c86766d2ce949537df5e7794cbfc0df9c25a7b
yogita77 <yogitapatil789@gmail.com>
2021-11-02 18:28:28 +0530

    Added Program file

---
+++ Reverseprogram.java
@@ -0,0 +1,21 @@
+
+package ReverseNumber;
+
+public class Reverseprogram
+{
+public static void main(String []agrs)
+{
+
+	String A="yogita Patil";
+	String Rev=" ";
+
+	for(int i=0;i<=A.length()-1;i++)
+	{
+		Rev=Rev+A.charAt(i);
+	}
+
+	System.out.println("original String :"+A);
+	System.out.println("Reverse String :"+Rev);
+
+}
+}
